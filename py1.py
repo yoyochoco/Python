35b5558a7f0e037d8e8039602921717ca79eacd9
yoyochoco <126222056+yoyochoco@users.noreply.github.com>
2023-02-24 12:52:48 +0800

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,15 @@
+#基本運算子練習
+#
+#
+x:int = 10                  #宣告x的type為int 且value指定為10
+y:int = 5                   #宣告y的type為int 且value指定為5
+
+
+print(x+y)                  #加法運算子 +
+print(x-y)                  #減法運算子 -
+print(x*y)                  #乘法運算子 *
+print(x/y)                  #除法運算子 /
+print(x//y)                 #截斷除法運算子(只顯示到整數位)
+print(x%y)                  #模數(mod)運算子(取餘數)
+print(-x)                   #一元減法
+print(+x)                   #一元加法
